484606213be9720ddf144c4e4d144d3ecdf20c94
Vincent Vega <56215164+TrueVincentVega@users.noreply.github.com>
2020-04-21 21:11:56 +0300

    Update dns-bind9.sh

--- dns-bind9.sh
+++ dns-bind9.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker run -d -it \
+    docker run -d -it \
     --name="dns-bind9" \
     --restart="unless-stopped" \
     --network="host" \
-    -p 53 \
-    -p 53/udp \
+    -P \
+    -v /etc/timezone:/etc/timezone:ro \
+    -v /etc/localtime:/etc/localtime:ro \
     -v /home/alex/docker/bind:/etc/bind \
     -v /var/log/named:/var/log/named \
-    -v /etc/localtime \
-    -v /etc/timezone \
     dns-bind9:latest
